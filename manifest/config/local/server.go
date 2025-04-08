55adf3cd54cc627942ad82340bf34fac8075ae9a
Charles <btp_charles@photons.com.tw>
2025-04-08 17:41:16 +0800

    chore 更新環境變數

--- manifest/config/local/server.go
+++ manifest/config/local/server.go
@@ -1,10 +1,18 @@
 package local
 
-import "linebot-go/manifest/config"
+import (
+	"github.com/gin-gonic/gin"
+	"linebot-go/manifest/config"
+	"os"
+)
 
 func CreateServerConfig() config.ServerConfig {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8081"
+	}
 	return config.ServerConfig{
-		HttpServer: &config.HttpServerConfig{Address: ":8081", Mode: "release"},
+		HttpServer: &config.HttpServerConfig{Address: ":" + port, Mode: gin.ReleaseMode},
 		LineConfig: &config.LineConfig{
 			ChannelId:     "2007224382",
 			ChannelSecret: "a0e83c3ca58f0ddc6c4157c7fa91ca28",
